f3fcaa4c443da5b3a9d08d9d5eff5795aeb8f10a
Kszymhu <czolczynski.k@gmail.com>
2023-06-18 02:12:36 +0200

    Add command line argument parsing

---
+++ src/cli.rs
@@ -0,0 +1,29 @@
+use std::path::PathBuf;
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "Carcinizer")]
+#[command(about = "Uses an evolutionary algorithm to change given string into \"crab\".")]
+#[command(long_about = "Treats given text (which has to consist only of ASCII characters, at the moment) similar to a nucleotide sequence, and performs \
+mutations such as insertions, deletions and substitutions to bring the text closer to the word \"crab\".")]
+pub struct Args {
+    #[arg(short = 'i')]
+    #[arg(long = "input")]
+    pub input: String,
+
+    #[arg(short = 'o')]
+    #[arg(long = "output_file")]
+    pub filename: PathBuf,
+
+    #[arg(short = 't')]
+    #[arg(long = "threads")]
+    #[arg(required = false)]
+    #[arg(default_value_t = 1)]
+    pub threads: usize,
+
+    #[arg(short = 'g')]
+    #[arg(long = "gen_size")]
+    #[arg(required = false)]
+    #[arg(default_value_t = 100)]
+    pub gen_size: usize
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod cli;
+
+use cli::Args;
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("input: {}", args.input);
+    println!("output file: {:?}", args.filename);
+    println!("thread count: {}", args.threads);
+    println!("generation size: {}", args.gen_size);
 }
